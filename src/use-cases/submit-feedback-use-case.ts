9b3465221ba3182240665396b1c31edcb5082b8c
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-02-05 03:58:18 -0300

    chore: created validation of parameters from request in use-case which submits feedback

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -17,6 +17,18 @@ export class SubmitFeedbackUseCase {
   async execute(request: SubmitFeedbackUseCaseRequest) {
     const { type, comment, screenshot } = request;
 
+    if (!type) {
+      throw new Error('Type is required!');
+    }
+
+    if (!comment) {
+      throw new Error('Comment is required!');
+    }
+
+    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+      throw new Error('Screenshot must be base64 encoded!');
+    }
+
     await this.feedbacksRepository.create({
       type,
       comment,
